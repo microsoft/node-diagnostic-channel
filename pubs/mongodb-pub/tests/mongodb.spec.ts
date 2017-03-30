9c99dff1c1ead2db828c08ac05ac2ffcf0485886
Jimmy Thomson <jithomso@microsoft.com>
2017-03-30 16:12:09 -0700

    Adding Zone to mongodb test

--- pubs/mongodb-pub/tests/mongodb.spec.ts
+++ pubs/mongodb-pub/tests/mongodb.spec.ts
@@ -6,6 +6,8 @@ import {mongodbcoreConnectionReplayPatchFunction} from './util/mongodbcore-mock-
 import '../mongodb-core.pub';
 import '../mongodb.pub';
 
+import 'zone.js';
+
 import * as fs from 'fs';
 import * as path from 'path';
 import * as assert from 'assert';
@@ -13,23 +15,12 @@ import * as assert from 'assert';
 describe('mongodb', function () {
 
     it('should fire events when we communicate with a collection, and preserve context', function (done) {
-        let context = 0;
-
         // Note:
         // We patch the underlying connection to record/replay a trace stored in util/mongodb.trace.json
         // This lets us validate the behavior of our mock as long as the mongo commands below are left unchanged,
         // or the trace is updated with a newly recorded version.
         channel.registerMonkeyPatch('mongodb-core', {versionSpecifier: "*", patch: mongodbcoreConnectionReplayPatchFunction});
-        channel.addContextPreservation((cb) => {
-                const originalContext = context;
-                return function () {
-                    const newContext = context;
-                    context = originalContext;
-                    const ret = cb.apply(this, arguments);
-                    context = newContext;
-                    return ret;
-                }
-        });
+        channel.addContextPreservation((cb) => Zone.current.wrap(cb, 'context preservation'));
 
         const events = [];
         channel.subscribe('mongodb', function (event) {
@@ -38,22 +29,20 @@ describe('mongodb', function () {
 
         let mongodb = require('mongodb');
 
-        context = 1;
+        const z1 = Zone.current.fork({name: '1'});
+        z1.run(() => 
         mongodb.MongoClient.connect("mongodb://localhost:27017/testdb", function (err, db) {
             if (err) {
                 done(err);
             }
             var collection = db.collection('documents');
 
-            /*
-            // TODO: test that context works with the assumptions we have made
-            if (context !== 1) {
-                done(new Error("Context not preserved in connect"));
-                return;
+            if (Zone.current != z1) {
+                return done(new Error('Context not preserved in connect'));
             }
-            */
 
-            context = 2;
+            const z2 = Zone.current.fork({name: '2'});
+            z2.run(() =>
             collection.insertMany([
                 {a: 1}, {a: 2}, {a: 3}
             ], function (err, result) {
@@ -65,20 +54,19 @@ describe('mongodb', function () {
                     done(new Error("Did not insert 3 elements"));
                     return;
                 }
-                /*
-                if (context !== 2) {
+                
+                if (Zone.current !== z2) {
                     done(new Error("Context not preserved in insert callback"));
                     return;
                 }
-                */
 
-                context = 3;
+                const z3 = Zone.current.fork({name: '3'});
+                z3.run(() => 
                 collection.deleteOne({a: 3}).then((result) => {
-                    /*
-                    if (context !== 3) {
+                    if (Zone.current !== z3) {
                         throw new Error("Context not preserved in delete promise");
                     }
-                    */
+
                     if (result.deletedCount !== 1) {
                         done(new Error("Did not delete one element"));
                         return;
@@ -96,10 +84,8 @@ describe('mongodb', function () {
                     assert.equal(events[1].data.succeeded, true);
 
 
-                }).then(done, done);
-            });
-        });
-
-        context = 4;
+                }).then(done, done));
+            }));
+        }));
     });
 });
