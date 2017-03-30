8984708815a51f691d72552f215cd702ea890c3a
Jimmy Thomson <jithomso@microsoft.com>
2017-03-30 11:08:46 -0700

    Adding tests to ensure that console subscribers are not reentrant

--- pubs/console-pub/tests/console.ts
+++ pubs/console-pub/tests/console.ts
@@ -1,46 +1,54 @@
 import * as assert from 'assert';
 
 // Need to undo any changes this require makes, so we can test that it makes them.
-const consolePropertyDescriptor = Object.getOwnPropertyDescriptor(global, 'console');
+const originalConsoleDescriptor = Object.getOwnPropertyDescriptor(global, 'console');
 import {console as consolePatch, ConsoleData} from "../console.pub";
-Object.defineProperty(global, 'console', consolePropertyDescriptor);
+Object.defineProperty(global, 'console', originalConsoleDescriptor);
 
 import {channel, makePatchingRequire, IStandardEvent} from "pubsub-channel";
 
 describe('Console', function () {
+    const moduleModule = require('module');
+    const originalRequire = moduleModule.prototype.require;
+
     before(() => (<any>channel).reset());
     afterEach(() => {
         (<any>channel).reset();
+        moduleModule.prototype.require = originalRequire;
+        Object.defineProperty(global, 'console', originalConsoleDescriptor);
     })
 
-    it('should intercept console.log', function () {
-        const originalConsoleDescriptor = Object.getOwnPropertyDescriptor(global, 'console');        
-        const moduleModule = require('module');
-        const originalRequire = moduleModule.prototype.require;
+    it('should intercept console.log', function () {    
         let eventEmitted: IStandardEvent<ConsoleData>;
         channel.subscribe<ConsoleData>('console', (event) => eventEmitted = event);
-        try {
-            moduleModule.prototype.require = makePatchingRequire({
-                'console': [
-                    consolePatch
-                ]
-            });
 
-            console.log("Before mocking");
-            assert(!eventEmitted, 'Nothing should be hooked up yet');
+        moduleModule.prototype.require = makePatchingRequire({'console': [consolePatch]});
 
-            const testLogMessage = "After mocking";
+        console.log("Before mocking");
+        assert(!eventEmitted, 'Nothing should be hooked up yet');
 
-            require('console');
+        const testLogMessage = "After mocking";
 
-            console.log(testLogMessage);
-            assert(eventEmitted, 'Event not published');
-            assert.equal(eventEmitted.data.message.replace('\n',''), testLogMessage);
-            assert(!eventEmitted.data.stderr);
+        require('console');
 
-        } finally {
-            Object.defineProperty(global, 'console', originalConsoleDescriptor);
-            moduleModule.prototype.require = originalRequire;
-        }
-    })
+        console.log(testLogMessage);
+        assert(eventEmitted, 'Event not published');
+        assert.equal(eventEmitted.data.message.replace('\n',''), testLogMessage);
+        assert(!eventEmitted.data.stderr);
+    });
+
+    it('should be safe to console.log within a console subscriber', function () {
+        let eventEmitted: IStandardEvent<ConsoleData>;
+        channel.subscribe<ConsoleData>('console', (event) => {
+            console.log('Logging within subscriber: ' + event.data.message);
+            eventEmitted = event;
+        });
+
+        moduleModule.prototype.require = makePatchingRequire({'console': [consolePatch]});
+
+        require('console');
+
+        console.log('Checking for crashes');
+        assert(eventEmitted, 'Event not published');
+    });
 });
