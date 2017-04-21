ec6f6a309d5c4b89daef2cc333cd91beb8f1e49e
Mike Kaufman <mike-kaufman@users.noreply.github.com>
2017-04-21 14:24:27 -0700

    Moving product source into src/ subdirectory, and sample code into sample/ (#4)
    * moving all source files to src directory and sample files to sample directory
    
    * fixing up paths in sample package.json

--- install.cmd
+++ install.cmd
@@ -3,21 +3,21 @@ rem Helper script to compile/install the packages in the correct order
 setlocal enableDelayedExpansion
 
 echo Installing DiagnosticsSource
-pushd %~dp0\diagnostic-source
+pushd %~dp0\src\diagnostic-source
 cmd.exe /c "npm install && npm run clean && npm test"
 popd
 
-for /F %%x in ('dir /B/D %~dp0\pubs') do (
+for /F %%x in ('dir /B/D %~dp0\src\pubs') do (
     echo ----------------------- Installing %%x
-    pushd %~dp0\pubs\%%x
+    pushd %~dp0\src\pubs\%%x
     cmd.exe /c "npm install && npm run clean && npm test"
     popd
 )
 
-for /F %%x in ('dir /B/D %~dp0\subs') do (
+for /F %%x in ('dir /B/D %~dp0\src\subs') do (
     IF NOT "%%x"==".gitignore" (
         echo -------------------- Installing %%x
-        pushd %~dp0\subs\%%x
+        pushd %~dp0\src\subs\%%x
         cmd.exe /c "npm install && npm test"
         popd
     )
--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash #
 # Helper script to compile/install the packages in the correct order
 BASEDIR=$(dirname "$0")
-pushd $BASEDIR/diagnostic-source
+pushd $BASEDIR/src/diagnostic-source
 npm install && npm run clean && npm test
 popd
 
-for x in `ls $BASEDIR/pubs`; do
-    pushd $BASEDIR/pubs/$x
+for x in `ls $BASEDIR/src/pubs`; do
+    pushd $BASEDIR/src/pubs/$x
     npm install && npm run clean && npm test
     popd
 done
 
-for x in `ls $BASEDIR/subs`; do
+for x in `ls $BASEDIR/src/subs`; do
     if [ ! $x == ".gitignore" ]
     then
-        pushd $BASEDIR/subs/$x
+        pushd $BASEDIR/src/subs/$x
         npm install && npm test
         popd
     fi
