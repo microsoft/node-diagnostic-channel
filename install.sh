bd64448519757f18e575ea55fefaa06ef7307ff0
Josh Gavant <josh.gavant@outlook.com>
2017-05-02 08:58:42 -0700

    update install scripts
    * Put `clean` before `install`.
    * Build `subs/ai-subs` after individual subs in `install.sh`.

--- install.sh
+++ install.sh
@@ -2,18 +2,24 @@
 # Helper script to compile/install the packages in the correct order
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $BASEDIR/src/diagnostic-channel
-npm install && npm run clean && npm test
+npm run clean && npm install && npm test
 popd
 
 pushd $BASEDIR/src/diagnostic-channel-publishers
-npm install && npm run clean && npm test
+npm run clean && npm install && npm test
 popd
 
+
 for x in `ls $BASEDIR/src/subs`; do
-    if [ ! $x == ".gitignore" ]
+    if [ ! $x == ".gitignore" -a ! $x == "ai-subs" ]
     then
         pushd $BASEDIR/src/subs/$x
-        npm install && npm test
+        npm run clean && npm install && npm test
         popd
     fi
 done
+
+pushd $BASEDIR/src/subs/ai-subs
+npm install && npm test
+popd
+
