2ec2b7c98601eb0c8f9dcd0833bfbbdf71582813
Mark Wolff <mark.wolff@microsoft.com>
2019-10-30 15:29:34 -0700

    refactor: Expose scope manager in channel

--- src/diagnostic-channel-publishers/tests/azure-coretracing.spec.ts
+++ src/diagnostic-channel-publishers/tests/azure-coretracing.spec.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 import * as coreTracingTypes from "@azure/core-tracing";
 import * as assert from "assert";
-import {channel, IStandardEvent, ParentOperationContextPreservationFunction} from "diagnostic-channel";
+import {channel, IStandardEvent} from "diagnostic-channel";
 import {AzureMonitorSymbol, enable as enableAzureSDKTracing} from "../src/azure-coretracing.pub";
 
 const assertSpans = (events, span) => {
@@ -16,11 +16,6 @@ describe("@azure/core-tracing@1.0.0-preview4+", () => {
     let coretracing: typeof coreTracingTypes;
     let events: Array<IStandardEvent<coreTracingTypes.Span>>;
     let tracer: coreTracingTypes.Tracer;
-    const traceparent = {
-        traceId: "242b113d433edc752d80a86cf28af791",
-        spanId: "87f1f98fbe876c24",
-    };
-    const mockContextPreservationFunction: ParentOperationContextPreservationFunction = () => traceparent;
 
     before(() => {
         enableAzureSDKTracing();
@@ -42,14 +37,5 @@ describe("@azure/core-tracing@1.0.0-preview4+", () => {
         assertSpans(events, span);
         assert.deepEqual(span["events"][0].name, "Application Insights Integration enabled");
         done();
-    });
-
-    it("should propagate context across async span contexts", () => {
-        assert.equal(tracer[AzureMonitorSymbol], true);
-        channel.setParentOperationContextPreservationFunction(mockContextPreservationFunction);
-        const span = tracer.startSpan("test span 1");
-        assert.equal(span.context().traceId, traceparent.traceId);
-        assert.equal(span["parentSpanId"], traceparent.spanId);
-        assert.notEqual(span.context().spanId, traceparent.spanId);
     });
 });
