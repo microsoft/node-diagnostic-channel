ecda8ae81f56d1fdfe742fa1f7f78be0b1fcb66d
Mark Wolff <mark.wolff@microsoft.com>
2020-05-05 16:20:17 -0400

    use latest azure sdk

--- src/diagnostic-channel-publishers/.sink.d.ts
+++ src/diagnostic-channel-publishers/.sink.d.ts
@@ -10,5 +10,12 @@ export interface Tracer {
     [wildcard: string]: any;
 }
 
+export type SDKRegistrationConfig = any;
+export class BasicTracerProvider {
+    register: any;
+}
+
+export const trace: any;
+
 export const setTracer: Function;
 export const getTracer: Function;
