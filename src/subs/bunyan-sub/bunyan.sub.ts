0cb95f7f181b029fd8f751e9b2b64807760df4e4
Hector Hernandez <39923391+hectorhdzg@users.noreply.github.com>
2025-11-14 16:47:44 -0800

    Update

--- src/subs/bunyan-sub/bunyan.sub.ts
+++ src/subs/bunyan-sub/bunyan.sub.ts
@@ -1,18 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 import ApplicationInsights = require("applicationinsights");
+import Contracts = require("applicationinsights/out/Declarations/Contracts");
 import {channel, IStandardEvent} from "diagnostic-channel";
 import {bunyan} from "diagnostic-channel-publishers";
-import {SeverityLevel} from "applicationinsights/out/Declarations/Contracts";
 
 // Mapping from bunyan levels defined at https://github.com/trentm/node-bunyan/blob/master/lib/bunyan.js#L256
 const bunyanToAILevelMap = {};
-bunyanToAILevelMap[10] = SeverityLevel.Verbose;
-bunyanToAILevelMap[20] = SeverityLevel.Verbose;
-bunyanToAILevelMap[30] = SeverityLevel.Information;
-bunyanToAILevelMap[40] = SeverityLevel.Warning;
-bunyanToAILevelMap[50] = SeverityLevel.Error;
-bunyanToAILevelMap[60] = SeverityLevel.Critical;
+bunyanToAILevelMap[10] = Contracts.SeverityLevel.Verbose;
+bunyanToAILevelMap[20] = Contracts.SeverityLevel.Verbose;
+bunyanToAILevelMap[30] = Contracts.SeverityLevel.Information;
+bunyanToAILevelMap[40] = Contracts.SeverityLevel.Warning;
+bunyanToAILevelMap[50] = Contracts.SeverityLevel.Error;
+bunyanToAILevelMap[60] = Contracts.SeverityLevel.Critical;
 
 export const subscriber = (event: IStandardEvent<bunyan.IBunyanData>) => {
     if (ApplicationInsights.defaultClient) {
--- src/subs/console-sub/console.sub.ts
+++ src/subs/console-sub/console.sub.ts
@@ -1,17 +1,17 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 import ApplicationInsights = require("applicationinsights");
+import Contracts = require("applicationinsights/out/Declarations/Contracts");
 
 import {channel, IStandardEvent} from "diagnostic-channel";
 
 import {console as consolePub} from "diagnostic-channel-publishers";
-import {SeverityLevel} from "applicationinsights/out/Declarations/Contracts";
 
 export const subscriber = (event: IStandardEvent<consolePub.IConsoleData>) => {
     if (ApplicationInsights.defaultClient) {
         const severity = event.data.stderr
-            ? SeverityLevel.Warning
-            : SeverityLevel.Information;
+            ? Contracts.SeverityLevel.Warning
+            : Contracts.SeverityLevel.Information;
         ApplicationInsights.defaultClient.trackTrace({message: event.data.message, severity: severity});
     }
 };
