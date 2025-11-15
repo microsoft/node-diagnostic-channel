77229af61760de86e1e0202707164d79d52fa0a6
Hector Hernandez <39923391+hectorhdzg@users.noreply.github.com>
2025-11-14 16:56:35 -0800

    CI changes

--- src/subs/console-sub/console.sub.ts
+++ src/subs/console-sub/console.sub.ts
@@ -1,17 +1,25 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 import ApplicationInsights = require("applicationinsights");
-import Contracts = require("applicationinsights/out/Declarations/Contracts");
 
 import {channel, IStandardEvent} from "diagnostic-channel";
 
 import {console as consolePub} from "diagnostic-channel-publishers";
 
+// SeverityLevel enum values for Application Insights
+enum SeverityLevel {
+    Verbose = 0,
+    Information = 1,
+    Warning = 2,
+    Error = 3,
+    Critical = 4
+}
+
 export const subscriber = (event: IStandardEvent<consolePub.IConsoleData>) => {
     if (ApplicationInsights.defaultClient) {
         const severity = event.data.stderr
-            ? Contracts.SeverityLevel.Warning
-            : Contracts.SeverityLevel.Information;
+            ? SeverityLevel.Warning
+            : SeverityLevel.Information;
         ApplicationInsights.defaultClient.trackTrace({message: event.data.message, severity: severity});
     }
 };
